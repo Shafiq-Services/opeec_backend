c11d5edd106b92e4fae6869476bc56be2f3e0a89
Shafiq Ahamd <officialshafiqahmad@gmail.com>
2025-06-15 16:21:42 +0500

    Chat Route Updated MIddleware

--- middleWares/common.js
+++ middleWares/common.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');  
-const User = require('../models/User');  
+const User = require('../models/user');  
 const Admin = require('../models/admin');  
 
 module.exports.commonMiddleware = async (req, res, next) => {
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { userMiddleware } = require("../middleWares/user");
-const { adminMiddleware } = require("../middleWares/adminMiddleWare");
+const { commonMiddleware } = require("../middleWares/common");
 
 const {
   getMessages,
@@ -15,15 +14,15 @@ const {
 } = require("../controllers/chatController");
 
 // Chat routes (user middleware applied)
-router.get("/conversations", userMiddleware, getConversations);
-router.get("/messages", userMiddleware, getMessages);
-router.post("/send", userMiddleware, sendMessage);
+router.get("/conversations", commonMiddleware, getConversations);
+router.get("/messages", commonMiddleware, getMessages);
+router.post("/send", commonMiddleware, sendMessage);
 
 // Support chat routes (user middleware applied)
-router.post("/support/send", userMiddleware, sendSupportMessage);
-router.get("/support/messages", userMiddleware, getSupportMessages);
+router.post("/support/send", commonMiddleware, sendSupportMessage);
+router.get("/support/messages", commonMiddleware, getSupportMessages);
 
 // Online users (user middleware applied)
-router.get("/online-users", userMiddleware, getOnlineUsers);
+router.get("/online-users", commonMiddleware, getOnlineUsers);
 
 module.exports = router;
